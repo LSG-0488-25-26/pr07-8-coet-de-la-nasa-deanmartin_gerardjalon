82b61294881f9d69869c777d1c794ec7e2ac102b
Gerard <gerardjalon@gmail.com>
2026-01-23 11:34:44 +0100

    feat: add MonsterEntity entity and column info

--- app/src/main/java/com/example/consumoapidungeondragons/db/MonsterEntity.kt
+++ app/src/main/java/com/example/consumoapidungeondragons/db/MonsterEntity.kt
@@ -1,8 +1,12 @@
 package com.example.consumoapidungeondragons.db
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+@Entity(tableName = "monsters")
 data class MonsterEntity(
     @PrimaryKey val index: String,
-    val name: String
+    val name: String,
+    @ColumnInfo(name = "is_killed") var isKilled: Boolean = false
 )
